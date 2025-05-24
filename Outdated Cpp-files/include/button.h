a3980bbdb922bd2264b1695c17d855d4911be079
Emil Pfeiffer Kristensen <emil@pfkr.dk>
2025-05-24 10:30:29 +0200

    removed old Cpp-files

--- Outdated Cpp-files/include/button.h
+++
@@ -1,7 +0,0 @@
-#include <LinkedList.h>
-
-#define SIZE_OF_BUTTON_ARRAY 3
-
-void updateButtonStateList(bool state);
-bool readButton();
-bool toggleButtonState(bool buttonState);
--- Outdated Cpp-files/include/tracking.h
+++
@@ -1,5 +0,0 @@
-#include <TinyGPSPlus.h>
-#include "main.h"
-void getPos(uint64_t* lastGPSTime, uint64_t* GPSInterval, HardwareSerial *gpsSerial, TinyGPSPlus *gps, float *pos, int8_t mode);
-bool GNSS(HardwareSerial *gpsSerial, TinyGPSPlus *gps, float *pos);
-bool WIFI_scanning(float* pos);
